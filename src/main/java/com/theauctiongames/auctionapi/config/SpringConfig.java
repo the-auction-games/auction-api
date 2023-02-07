92a0f18dba187bafc288b26c8b3d4ff36853ac1d
Joel Smith <joelsmith.2019@gmail.com>
2023-02-06 18:14:53 -0700

    Enable CORS on all endpoints

--- src/main/java/com/theauctiongames/auctionapi/config/SpringConfig.java
+++ src/main/java/com/theauctiongames/auctionapi/config/SpringConfig.java
@@ -1,11 +1,25 @@
 package com.theauctiongames.auctionapi.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * The spring configuration class.
  */
 @Configuration
-public class SpringConfig {
-    // Currently Unused
+public class SpringConfig implements WebMvcConfigurer {
+
+    /**
+     * Add CORS mapping to all endpoints.
+     *
+     * @param registry the registry
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
 }
